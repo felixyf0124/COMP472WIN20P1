eb2dd1c9618b4da48866278e7465e564bf973bc2
felixyf0124 <felixyf0124@gmail.com>
2020-02-04 07:31:33 -0500

    #4 added dot puzzle class with initial and display

--- DotPuzzle/DotPuzzle.py
+++ DotPuzzle/DotPuzzle.py
@@ -1,4 +1,46 @@
 import numpy as np
 
-x = np.array([1,2,3])
-print(x)
+class DotPuzzle:
+    def __init__(self, n):
+        self.n = int(n)
+        self.board = np.random.rand(n,n)
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                x = self.board[i][j]
+                y = 0.5
+                if x >= y:
+                    self.board[i][j] = 1
+                else:
+                    self.board[i][j] = 0
+        self.board = self.board.astype(int)
+        
+    def display(self):
+        out = "\n  |"
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for i in range(len(self.board[0])):
+            out += " " + str(i+1)
+
+        out += "\n--|"
+
+        for i in range(len(self.board[0])):
+            out += "--"
+
+        out += "\n"
+
+        for i in range(len(self.board)):
+            out += " " + alpha[i] + "|"
+            for j in range(len(self.board[i])):
+                x = self.board[i][j]
+                y = 1
+                if x == y:
+                    out += " X"
+                else:
+                    out += " O"
+            out+="\n"
+        print(out)
+    
+   
+# test  
+p = DotPuzzle(3)
+print(p.board)
+p.display()
