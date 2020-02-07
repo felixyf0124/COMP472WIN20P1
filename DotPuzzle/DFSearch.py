760cb54e53e66f0abc4645bba05c24e1f409e7e8
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-02-07 00:30:59 -0500

    Added pointer for depth in the DFS
    Co-Authored-By: felixyf0124 <felixyf0124@gmail.com>

--- DotPuzzle/DFSearch.py
+++ DotPuzzle/DFSearch.py
@@ -1,4 +1,3 @@
-
 # class DFSearch:
 #     def __init__(self, max_d):
 #         self.closedList = []
@@ -6,31 +5,30 @@
 #         self.current_d = 1
 #         self.max_d = max_d
 
-
 # Using a Python dictionary to act as an adjacency list
 graph = {
     'A': ['B', 'C'],
     'B': ['D', 'E'],
-    'C': ['F'],
+    'C': [],
     'D': [],
     'E': ['F'],
     'F': []
 }
 
 visited = []  # Array to keep track of visited nodes.
+MAX_DEPTH = 3
+depth = 0
 
 
-def dfs(visited, graph, node):
-    MAX_DEPTH = 3
-    depth = 0
+def dfs(visited, graph, node, depth):
     if node not in visited:
         print(str(node))
-        print(depth)
         visited.append(node)
-        depth += 1
         for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+            depth += 1
+            print(depth)
+            dfs(visited, graph, neighbour, depth)
 
 
 # Driver Code
-dfs(visited, graph, 'A')
+dfs(visited, graph, 'A', depth)
