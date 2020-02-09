05f26064abd71fd406ea5f0cf71462914bfb6b92
felixyf0124 <felixyf0124@gmail.com>
2020-02-09 12:57:39 -0500

    #3 temp save update

--- DotPuzzle/DFS.py
+++ DotPuzzle/DFS.py
@@ -10,6 +10,7 @@ class DFS:
         self.current_d = 0
         self.max_d = max_d
         self.virtualDP = dp.DotPuzzle(2)  # here can be any number. after import it will be replaced
+        self.isSolFound = False
 
     # add initial root state also update initial step for solution
     def addRoot(self, rootState):
@@ -19,8 +20,26 @@ class DFS:
         self.virtualDP.import1DState(rootState)
 
     def doSearch(self):
-        # self.virtualDP.touch()
-        pass
+        # TODO 
+        if(self.current_d >= self.max_d):
+            # back 1 depth up
+        # pick a dot  
+        dot = ["y", 0]
+        # try touch dot
+        self.virtualDP.touch(dot[0],dot[1])
+        nextState = self.virtualDP.get1DState()
+        if(self.isVisited(nextState)):
+            # back to last state by touching same dot
+            self.virtualDP.touch(dot[0],dot[1])
+        else:
+            self.current_d += 1
+            self.visited.append(nextState)
+            self.closedList.append(nextState)
+            self.solution.append([dot[0]+str(dot[1]), nextState])
+            if(self.isGoalState(nextState)):
+                break
+        
+
 
     # check if the state is visited
     def isVisited(self, stateNode):
@@ -31,4 +50,20 @@ class DFS:
                 break
         return isVisited
 
-    # def getBestBreadth(self,)
+    # check if state is a goal state 
+    # and update isSolFound as True
+    # and return bool
+    def isGoalState(self,state):
+        goalState = state.replace("1","0")
+        if(state == goalState):
+            self.isSolFound = True
+            return True
+        else:
+            return False
+
+    # go back to parent state of current state
+    # def backToParentState(self):
+    #     lastTouch = self.solution[len(self.solution)-1]
+    #     self.virtualDP.touch(lastTouch[0])
+
+    # TODO generateCurrentDepthAvailableState
