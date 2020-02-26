70f2b961f1eee330f151b4f5492e19c393a38a84
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-02-26 11:55:56 -0500

    #9 Loop has been implemented to loop through the puzzles.

--- DotPuzzle/Loader.py
+++ DotPuzzle/Loader.py
@@ -1,26 +1,31 @@
 
 import PuzzleAdapter as PA
 
+
 class Loader:
     def __init__(self, path):
         self.myPuzzleList = []
-        #self.numPuzzles=0
-        #number of lines in the input file, aka. number of puzzles to be solved
+        # self.numPuzzles=0
+        # number of lines in the input file, aka. number of puzzles to be solved
         file = open(path, "r")
         for line in file:
             c = line.split(" ")
-            c[3] = c[3].replace('\n','')
-            myPuzzle = PA.PuzzleAdapter(c[0],c[1],c[2],c[3])
+            c[3] = c[3].replace('\n', '')
+            myPuzzle = PA.PuzzleAdapter(c[0], c[1], c[2], c[3])
             self.myPuzzleList.append(myPuzzle)
-            #self.numPuzzles+1
+            # self.numPuzzles+1
         file.close()
 
-    #get specific puzzle at index
+    # get specific puzzle at index
     # tested
     def getMyPuzzleAt(self, index):
         return self.myPuzzleList[index]
 
-    #show all puzzle game list
+    # get size of list of puzzles in the path file
+    def getMyPuzzleSize(self):
+        return len(self.myPuzzleList)
+
+    # show all puzzle game list
     # tested
     def showMyPuzzleList(self):
         for i in range(len(self.myPuzzleList)):
@@ -32,8 +37,6 @@ class Loader:
             print(line)
 
 
-
-#test
+# test
 # l = Loader("../test.txt")
 # l.showMyPuzzleList()
-  
--- DotPuzzle/main.py
+++ DotPuzzle/main.py
@@ -3,22 +3,24 @@ import Loader as ld
 import SolutionWriter as wr
 
 # loads the test.txt input file.
-loader = ld.Loader("../test.txt")
-# loads puzzle #1
-initSet = loader.getMyPuzzleAt(0)
-dfSearcher = dfs.DFS(initSet.getMaxDeepth(), False)
-dfSearcher.addRoot(initSet.get1DState())
-# runs search using DFS method
-dfSearcher.doSearch()
-print("FINAL SOLUTION")
-print(dfSearcher.solution)
-print("SOL FOUND?")
-print(dfSearcher.isSolFound)
-solution = dfSearcher.getFinalSolution()
-search = dfSearcher.getSearchPath()
-print(solution)
-# prints output
-writer = wr.SolutionWriter(1, "dfs")
-writer.createSearchSolutionFile("solution", solution)
-writer.createSearchSolutionFile("search", search)
+loader = ld.Loader("test.txt")
 
+# get solution for dfs
+for i in range(loader.getMyPuzzleSize()):
+    # loads puzzle #1
+    initSet = loader.getMyPuzzleAt(i)
+    dfSearcher = dfs.DFS(initSet.getMaxDeepth(), False)
+    dfSearcher.addRoot(initSet.get1DState())
+    # runs search using DFS method
+    dfSearcher.doSearch()
+    print("FINAL SOLUTION")
+    print(dfSearcher.solution)
+    print("SOL FOUND?")
+    print(dfSearcher.isSolFound)
+    solution = dfSearcher.getFinalSolution()
+    search = dfSearcher.getSearchPath()
+    print(solution)
+    # prints output
+    writer = wr.SolutionWriter(i, "dfs")
+    writer.createSearchSolutionFile("solution", solution)
+    writer.createSearchSolutionFile("search", search)
