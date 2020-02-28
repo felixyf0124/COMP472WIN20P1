2c5d269d8bb95f332f7ea3cc5a3df622ee490fdf
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-02-27 20:54:07 -0500

    #7 Added heuristics for BFS. Next: change DFS algo to BFS algo.

---
+++ DotPuzzle/BFS.py
@@ -0,0 +1,222 @@
+import DotPuzzle as dp
+import numpy as np
+
+
+class BFS:
+    # constructor add Max depth limit
+    def __init__(self, max_d, debug):
+        self.closedList = []  # = search path
+        self.openList = []
+        self.visited = []
+        self.solution = []
+        self.current_d = 0
+        self.max_d = max_d
+        self.debug = debug
+        # here can be any number. after import it will be replaced
+        self.virtualDP = dp.DotPuzzle(3)
+        self.isSolFound = False
+        self.counter = 0
+
+    # add initial root state also update initial step for solution
+    def addRoot(self, rootState):
+        self.openList.append(
+            ["0", 0, rootState, self.getHeuristics(rootState)])
+        self.openList = sorted(self.openList, key=lambda x: x[-1])
+        self.solution.append(
+            ["0", 0, rootState, self.getHeuristics(rootState)])
+        self.current_d = 1
+        self.virtualDP.import1DState(rootState)
+
+    def doSearch(self):
+        # TODO add one more check if low level dept available state is token by the high level one
+        self.counter += 1
+
+        if(self.debug):
+            print("======================================")
+            print("CURRENT DEPTH")
+            print(self.current_d)
+            print("CURRENT GAME STATE")
+            print(self.virtualDP.get1DState())
+            print("VISITED")
+            print(self.visited)
+            print("OPEN LIST")
+            print(self.openList)
+            print("CLOSED LIST")
+            print(self.closedList)
+            print("SOLUTION")
+            print(self.solution)
+
+        available = self.openList[0]
+
+        if (available[0] == "0"):
+            self.visited = available[0]
+            self.closedList.append(available)
+            del self.openList[0]
+            nextAvailable = self.generateNextAvailableState()
+
+            self.openList.extend(nextAvailable)
+            for i in range(len(nextAvailable)):
+                self.doSearch()
+                if(self.isSolFound):
+                    return
+            return
+        else:
+            # pick a dot
+            dot = [available[0], available[1]]
+            # try touch dot
+            self.virtualDP.touch(dot[0], dot[1])
+            nextState = self.virtualDP.get1DState()
+            if(self.isClosed(nextState)):  # the same state might be token by parallel's children
+                # back to last state by touching same dot
+                self.virtualDP.touch(dot[0], dot[1])
+                return
+            elif(nextState != available[2]):
+                self.virtualDP.touch(dot[0], dot[1])
+                return
+                # raise Exception(
+                #     "Error: touch dot not matching the associated state")
+            else:
+                self.current_d += 1
+                self.visited = available
+                if(self.isClosed(available[2])):
+                    self.closedList.append(available)
+                del self.openList[0]
+                self.solution.append(
+                    [dot[0], dot[1], nextState, self.getHeuristics(nextState)])
+                if(self.counter % 10000 == 0):
+                    print("SOLUTION")
+                    print(self.solution)
+                if(self.isGoalState(nextState)):
+                    return
+                # if it reached the max depth go back for deepest parallel level
+                elif(self.current_d >= self.max_d):
+
+                    # back 1 depth up
+                    lastTouch = self.solution[-1]
+                    self.virtualDP.touch(lastTouch[0], lastTouch[1])
+                    lastState = self.virtualDP.get1DState()
+                    del self.solution[len(self.solution)-1]
+                    self.current_d -= 1
+                    if (lastState != self.solution[len(self.solution)-1][2]):
+                        raise Exception(
+                            "Error: return back to last state failed")
+                    return
+                else:  # it does not reach the max depth limit yet
+                    nextAvailable = self.generateNextAvailableState()
+                    self.openList[0:0] = nextAvailable
+                    for i in range(len(nextAvailable)):
+                        self.doSearch()  # DFS here 1 level deeper
+                        if(self.isSolFound):
+                            return
+                    # no solution found so return back
+                    # back 1 depth up
+                    lastTouch = self.solution[-1]
+                    self.virtualDP.touch(lastTouch[0], lastTouch[1])
+                    lastState = self.virtualDP.get1DState()
+                    del self.solution[len(self.solution)-1]
+                    self.current_d -= 1
+                    return
+
+    # check if the state is closed
+
+    def isClosed(self, stateNode):
+        isClosed = False
+        for i in range(len(self.closedList)):
+            if(stateNode == self.closedList[i]):
+                isClosed = True
+                break
+        return isClosed
+
+    # check if state is a goal state
+    # and update isSolFound as True
+    # and return bool
+    def isGoalState(self, state):
+        goalState = state.replace("1", "0")
+        if(state == goalState):
+            self.isSolFound = True
+            return True
+        else:
+            return False
+
+    # return next available state with associated dot in the same depth level
+    # TODO option: make a parent class and move this to parent class?
+    def generateNextAvailableState(self):
+        if(self.debug):
+            print("====================================generateNextAvailableState")
+        lastTouch = self.solution[-1]
+        nextAvailable = []
+        puzzleM = dp.DotPuzzle(3)
+        puzzleM.import1DState(lastTouch[2])
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for j in range(puzzleM.n):
+            for i in range(puzzleM.n):
+                dot = [alpha[j], i+1]
+                # touch it so we can get the next state
+                puzzleM.touch(dot[0], dot[1])
+                state1D = puzzleM.get1DState()
+                available = [dot[0], dot[1], state1D,
+                             self.getHeuristics(state1D)]
+                nextAvailable.append(available)  # add to nextAvailable list
+                nextAvailable = sorted(
+                    nextAvailable, key=lambda x: x[-1], reverse=True)
+                # touch it again so it go back to the last state (parent)
+                puzzleM.touch(dot[0], dot[1])
+
+        # so far we got all same level states with dot positions to touch
+        # we have to filter what is not available in the list
+        # as long as it is not the root state, the parent state's touch dot should be unavailable
+        # because touching the same dot will go back to its last state (grand parent)
+        # benefit of filtering this is because its max O(n) is n x n, size of the board
+        # which is fixed and much less than O(n) of iterating the closed list after a while of search
+        if(self.debug):
+            print("-------------------------------------------------------")
+            print(lastTouch)
+            print(nextAvailable)
+            print("-------------------------------------------------------")
+
+        if(lastTouch[0] != "0"):
+            for i in range(len(nextAvailable)):
+                if(lastTouch[0] == nextAvailable[i][0] and lastTouch[1] == nextAvailable[i][1]):
+                    del nextAvailable[i]
+                    break
+        # del list
+        unavailableIndex = []
+        for i in range(len(nextAvailable)):
+            if(self.isClosed(nextAvailable[i][2])):
+                unavailableIndex.append(i)
+
+        # del from the last to the first so we won't be bothered by the changed subsequence
+        for i in sorted(unavailableIndex, reverse=True):
+            del nextAvailable[i]
+        if(self.debug):
+            print("next available")
+            print(nextAvailable)
+
+        return nextAvailable
+
+    # generates the heuristic value
+    def getHeuristics(self, state):
+        return state.count('0')
+
+    # return null if not found
+    # else return final solution
+    def getFinalSolution(self):
+        solution = "No Solution"
+        if(self.isSolFound):
+            solution = ""
+            for i in range(len(self.solution)):
+                if(self.solution[i][0] == "0"):
+                    solution += self.solution[i][0]
+                else:
+                    solution += self.solution[i][0] + str(self.solution[i][1])
+                solution += " " + self.solution[i][2] + "\n"
+
+        return solution
+
+    # return search path
+    def getSearchPath(self):
+        searchPath = ""
+        for i in range(len(self.closedList)):
+            searchPath += "0 0 0 " + self.closedList[i][2] + "\n"
+
+        return searchPath
--- DotPuzzle/main.py
+++ DotPuzzle/main.py
@@ -1,26 +1,57 @@
+import BFS as bfs
 import DFS as dfs
 import Loader as ld
 import SolutionWriter as wr
+import time
 
 # loads the test.txt input file.
 loader = ld.Loader("test.txt")
 
 # get solution for dfs
+# for i in range(loader.getMyPuzzleSize()):
+#     # calculate the time
+#     start = time.time()
+#     # loads puzzle #1
+#     initSet = loader.getMyPuzzleAt(i)
+#     dfSearcher = dfs.DFS(initSet.getMaxDeepth(), False)
+#     dfSearcher.addRoot(initSet.get1DState())
+#     # runs search using DFS method
+#     dfSearcher.doSearch()
+#     print("FINAL SOLUTION")
+#     print(dfSearcher.solution)
+#     print("SOL FOUND?")
+#     print(dfSearcher.isSolFound)
+#     solution = dfSearcher.getFinalSolution()
+#     search = dfSearcher.getSearchPath()
+#     print(solution)
+#     # prints output
+#     writer = wr.SolutionWriter(i, "dfs")
+#     writer.createSearchSolutionFile("solution", solution)
+#     writer.createSearchSolutionFile("search", search)
+#     end = time.time()
+#     print(end - start)
+
+# get solution for bfs
 for i in range(loader.getMyPuzzleSize()):
+    # calculate the time
+    start = time.time()
     # loads puzzle #1
     initSet = loader.getMyPuzzleAt(i)
-    dfSearcher = dfs.DFS(initSet.getMaxDeepth(), False)
-    dfSearcher.addRoot(initSet.get1DState())
+    bfSearcher = bfs.BFS(initSet.getMaxDeepth(), False)
+    # bfSearcher = bfs.BFS(4, True)
+    bfSearcher.addRoot(initSet.get1DState())
     # runs search using DFS method
-    dfSearcher.doSearch()
+    bfSearcher.doSearch()
     print("FINAL SOLUTION")
-    print(dfSearcher.solution)
+    print(bfSearcher.solution)
     print("SOL FOUND?")
-    print(dfSearcher.isSolFound)
-    solution = dfSearcher.getFinalSolution()
-    search = dfSearcher.getSearchPath()
+    print(bfSearcher.isSolFound)
+    solution = bfSearcher.getFinalSolution()
+    search = bfSearcher.getSearchPath()
     print(solution)
     # prints output
-    writer = wr.SolutionWriter(i, "dfs")
+    writer = wr.SolutionWriter(i, "bfs")
     writer.createSearchSolutionFile("solution", solution)
     writer.createSearchSolutionFile("search", search)
+    end = time.time()
+    print(end - start)
